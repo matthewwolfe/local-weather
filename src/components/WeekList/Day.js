952d35ce9c4476c7fc906e4b1fb73ce3f51fad7b
matthewwolfe <matthew.wolfe2@gmail.com>
2020-07-04 12:56:57 -0400

    fixes to mobile layout

--- src/components/WeekList/Day.js
+++ src/components/WeekList/Day.js
@@ -2,18 +2,25 @@ import { Box, MenuItem, Typography } from '@material-ui/core';
 
 function Day({ day, narrative, temperatureMax, temperatureMin }) {
   return (
-    <Box display="flex" pt={6}>
-      <Box width={1 / 2}>
+    <Box display="flex" pt={6} flexDirection={['column', 'column', 'row']}>
+      <Box width={[1, 1, 2 / 3]}>
         <Typography variant="body1">{day}</Typography>
         <Typography variant="body2">{narrative}</Typography>
       </Box>
-      <Box width={1 / 4} textAlign="center">
-        {temperatureMax && (
-          <Typography variant="body1">{temperatureMax}&deg;F</Typography>
-        )}
-      </Box>
-      <Box width={1 / 4} textAlign="center">
-        <Typography variant="body1">{temperatureMin}&deg;F</Typography>
+
+      <Box
+        display={['flex', 'flex', 'box']}
+        mt={[4, 4, 0]}
+        width={[1, 1, 1 / 3]}
+      >
+        <Box width={[1, 1, 1 / 2]} textAlign={['left', 'left', 'center']}>
+          {temperatureMax && (
+            <Typography variant="body1">{temperatureMax}&deg;F</Typography>
+          )}
+        </Box>
+        <Box width={[1, 1, 1 / 2]} textAlign={['left', 'left', 'center']}>
+          <Typography variant="body1">{temperatureMin}&deg;F</Typography>
+        </Box>
       </Box>
     </Box>
   );
