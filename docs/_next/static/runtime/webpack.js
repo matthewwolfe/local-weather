1191da1d5acfe6001947de12cfa7447215e24e34
matthewwolfe <matthew.wolfe2@gmail.com>
2020-07-25 15:13:26 -0400

    prod build

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "15b5fea0dd1440e66aca";
+/******/ 	var hotCurrentHash = "7df278eaa144e1f8535c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
